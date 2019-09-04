c40e33dad4cacbae4e9d5ac419cc271de6b68273
unknown <bmotaa98@gmail.com>
2019-09-03 21:34:49 -0300

    atualizando projeto spring boot

---
+++ src/main/java/br/com/unifacef/api/entities/Empresa.java
@@ -0,0 +1,12 @@
+package br.com.unifacef.api.entities;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa implements Serializable {
+
+ private static final long serialVersionUID = 4862991749577621407L;
+
+}
--- src/test/java/br/com/unifacef/api/utils/SenhaUtils.java
+++
@@ -1,5 +0,0 @@
-package br.com.unifacef.api.utils;
-
-public class SenhaUtils {
-
-}
